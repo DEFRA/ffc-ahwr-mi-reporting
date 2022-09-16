67db5fbe282e2546e62614006c8b3404fa0a0d5f
steve-dickinson <steve.dickinson@defra.gov.uk>
2022-09-16 07:52:33 +0100

    Added time stamp

--- ffc-ahwr-mi-reporting/mi-report.js
+++ ffc-ahwr-mi-reporting/mi-report.js
@@ -41,6 +41,24 @@ const parseData = (events, type, key) => {
   }
 }
 
+const calculateJourneyTime = (events) => {
+  const sortEvents = events.sort((a, b) => new Date(a.EventRaised) - new Date(b.EventRaised))
+  const firstEvent = sortEvents[0].EventRaised
+  const lastEvent = sortEvents[sortEvents.length - 1].EventRaised
+  let timeDelta = Math.abs(new Date(firstEvent) - new Date(lastEvent)) / 1000
+  const days = Math.floor(timeDelta / 86400)
+  timeDelta -= days * 86400
+  const hours = Math.floor(timeDelta / 3600) % 24
+  timeDelta -= hours * 3600
+  const minutes = Math.floor(timeDelta / 60) % 60
+  timeDelta -= minutes * 60
+  const seconds = timeDelta % 60
+  return [
+    { text: 'Apply journey time\n\n', style: 'subheader' },
+    `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`
+  ]
+}
+
 const convertToCSV = (data) => {
   let csv = ''
   csv = data.map(row => Object.values(row))
@@ -55,6 +73,13 @@ const parseCsvData = (events) => {
   const whichReview = parseData(events, 'farmerApplyData-whichReview', 'whichReview')
   const eligibleSpecies = parseData(events, 'farmerApplyData-eligibleSpecies', 'eligibleSpecies')
   const confirmCheckDetails = parseData(events, 'farmerApplyData-confirmCheckDetails', 'confirmCheckDetails')
+  const agreementReference = parseData(events, 'farmerApplyData-reference', 'reference')
+
+  const claimDetailsCorrect = parseData(events, 'claim-detailsCorrect', 'detailsCorrect')
+  const claimVisitDate = parseData(events, 'claim-visitDate', 'visitDate')
+  const claimVetName = parseData(events, 'claim-vetName', 'vetName')
+  const claimVetRcvs = parseData(events, 'claim-vetRcvs', 'vetRcvs')
+  const claimClaimed = parseData(events, 'claim-claimed', 'claimed')
 
   return {
     sbi: organisation?.sbi,
@@ -64,8 +89,24 @@ const parseCsvData = (events) => {
     address: organisation?.address.replace(/,/g, '","'),
     email: organisation?.email,
     whichReview: whichReview?.value,
+    whichReviewRaisedOn: whichReview?.raisedOn,
     eligibleSpecies: eligibleSpecies?.value,
-    confirmCheckDetails: confirmCheckDetails?.value
+    eligibleSpeciesRaisedOn: eligibleSpecies?.raisedOn,
+    confirmCheckDetails: confirmCheckDetails?.value,
+    confirmCheckDetailsRaisedOn: confirmCheckDetails?.raisedOn,
+    confirmAgreementReview: agreementReference?.value ? 'yes' : '',
+    confirmAgreementReviewRaisedOn: agreementReference?.value ? agreementReference?.raisedOn : '',
+    applicationNumber: agreementReference?.value,
+    claimDetailsCorrect: claimDetailsCorrect?.value,
+    claimDetailsCorrectRaisedOn: claimDetailsCorrect?.raisedOn,
+    claimVisitDate: claimVisitDate?.value,
+    claimVisitDateRaisedOn: claimVisitDate?.raisedOn,
+    claimVetName: claimVetName?.value,
+    claimVetNameRaisedOn: claimVetName?.raisedOn,
+    claimVetRcvs: claimVetRcvs?.value,
+    claimVetRcvsRaisedOn: claimVetRcvs?.raisedOn,
+    claimClaimed: claimClaimed?.value,
+    claimClaimedRaisedOn: claimClaimed?.raisedOn
   }
 }
 
