60db1c1c62b295de4416d15c6af736d297d6cd3c
liamwilson-kainos <liam.wilson@kainos.com>
2023-02-09 13:37:30 +0000

    Included dynamic environment to MI report email subject

--- ffc-ahwr-mi-reporting/email/notify-send.js
+++ ffc-ahwr-mi-reporting/email/notify-send.js
@@ -1,12 +1,13 @@
 const notifyClient = require('./notify-client')
 const createFileName = require('../create-filename')
 const { downloadFile } = require('../storage')
-const { templateMiReport, miEmailAddress } = require('../config')
+const { templateMiReport, miEmailAddress, environment } = require('../config')
 
 const send = async () => {
   if (miEmailAddress) {
     const contents = await downloadFile(createFileName())
-    const personalisation = { link_to_file: notifyClient.prepareUpload(contents, true) }
+    const personalisation = { environment, link_to_file: notifyClient.prepareUpload(contents, true) }
+    console.log(`Sending MI report to email ${miEmailAddress} for environment ${environment}`)
     return notifyClient.sendEmail(
       templateMiReport,
       miEmailAddress,
