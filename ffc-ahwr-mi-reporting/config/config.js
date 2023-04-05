9e8485c3341a03d508bc7f956e63392afc3db8c9
Marcin Mogiela <marcinmo@kainos.com>
2023-04-05 11:39:22 +0200

    FFC-170616: remove notify from config

--- ffc-ahwr-mi-reporting/config/config.js
+++ ffc-ahwr-mi-reporting/config/config.js
@@ -2,7 +2,6 @@ module.exports = {
   connectionString: process.env.STORAGE_CONNECTION_STRING,
   containerName: 'reports',
   tableName: 'ahwreventstore',
-  notifyApiKey: process.env.NOTIFY_API_KEY,
   templateMiReport: process.env.MI_TEMPLATE_REPORT,
   templateEligibilityMiReport: process.env.ELIGIBILITY_MI_TEMPLATE_REPORT,
   miEmailAddress: process.env.MI_EMAIL_ADDRESS,
