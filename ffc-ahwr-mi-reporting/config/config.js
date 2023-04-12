68bab995b7cee4c651bf8bc17691905c6d4e4bc8
Marcin Mogiela <marcinmo@kainos.com>
2023-04-12 14:08:12 +0200

    FFC-170616: read sharePoint config only when enabled

--- ffc-ahwr-mi-reporting/config/config.js
+++ ffc-ahwr-mi-reporting/config/config.js
@@ -1,8 +1,10 @@
+const featureToggle = require('../feature-toggle/config')
+
 module.exports = {
   environment: process.env.ENVIRONMENT ?? 'unknown',
   connectionString: process.env.STORAGE_CONNECTION_STRING,
   containerName: 'reports',
   tableName: 'ahwreventstore',
-  sharePoint: require('../sharepoint/config'),
-  featureToggle: require('../feature-toggle/config')
+  sharePoint: featureToggle.sharePoint.enabled ? require('../sharepoint/config') : {},
+  featureToggle
 }
