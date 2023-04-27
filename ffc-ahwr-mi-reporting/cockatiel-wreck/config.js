a253848c8e7bcf6449dcca40409d244d4a6872cf
Marcin Mogiela <marcinmo@kainos.com>
2023-04-27 10:26:51 +0200

    remove comments

--- ffc-ahwr-mi-reporting/cockatiel-wreck/config.js
+++ ffc-ahwr-mi-reporting/cockatiel-wreck/config.js
@@ -1,6 +1,5 @@
 const Joi = require('joi')
 
-// Define config schema
 const schema = Joi.object({
   maxAttempts: Joi.number().optional().default(3),
   halfOpenAfter: Joi.number().optional().default(10 * 1000),
@@ -8,7 +7,6 @@ const schema = Joi.object({
   enabled: Joi.boolean().optional().default(false)
 })
 
-// Build config
 const config = {
   maxAttempts: process.env.COCKATIEL_MAX_ATTEMPTS,
   halfOpenAfter: process.env.COCKATIEL_HALF_OPEN_AFTER,
@@ -16,12 +14,10 @@ const config = {
   enabled: process.env.COCKATIEL_ENABLED
 }
 
-// Validate config
 const result = schema.validate(config, {
   abortEarly: false
 })
 
-// Throw if config is invalid
 if (result.error) {
   throw new Error(`The cockatiel config is invalid: ${result.error.message}`)
 }
