836ff7878bba3373bbf1ed2b72a26237d4c6a8e0
Marcin Mogiela <marcinmo@kainos.com>
2023-08-07 12:02:43 +0200

    fix test

--- test/unit/exception-mi-report/index.test.js
+++ test/unit/exception-mi-report/index.test.js
@@ -87,9 +87,7 @@ describe('AHWR Ineligibility MI Report', () => {
       expect(logSpy).toHaveBeenNthCalledWith(2, `${MOCK_NOW.toISOString()} ahwr-ineligibility-mi-report: CSV file has been uploaded to Azure Storage: ${JSON.stringify({
             fileName: 'fileName'
           })}`)
-      expect(logSpy).toHaveBeenNthCalledWith(3, `${MOCK_NOW.toISOString()} ahwr-ineligibility-mi-report: SharePoint upload disabled ${JSON.stringify({
-            fileName: 'fileName'
-          })}`)
+      expect(logSpy).toHaveBeenNthCalledWith(3, `${MOCK_NOW.toISOString()} ahwr-ineligibility-mi-report: SharePoint disabled`)
     })
   })
 
