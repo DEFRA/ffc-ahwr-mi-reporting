f9743ab01024b4f6bda2de75e9f20826435ba25f
JulieLev <JulieLev@users.noreply.github.com>
2024-03-18 12:26:00 +0000

    standardised test data

--- test/unit/mi-report-v2/transformJsonToCsv.test.js
+++ test/unit/mi-report-v2/transformJsonToCsv.test.js
@@ -5,27 +5,27 @@ const events = [{
   SessionId: '789123456',
   EventType: 'farmerApplyData-organisation',
   EventRaised: new Date().toISOString(),
-  Payload: '{"type":"farmerApplyData-organisation","message":"Session set for farmerApplyData and organisation.","data":{"reference":"TEMP-931B-C490","organisation":{"sbi":"106401373","farmerName":"Trevor John Hale","name":"M & G Williams","email":"trevorhalec@elahroverts.com.test","address":""}},"raisedBy":"trevorhalec@elahroverts.com.test","raisedOn":"2024-02-15T13:23:57.287Z"}'
+  Payload: '{"type":"farmerApplyData-organisation","message":"Session set for farmerApplyData and organisation.","data":{"reference":"TEMP-1234-ABCD","organisation":{"sbi":"123456","farmerName":"Farmer Brown","name":"Brown Cow Farm","email":"brown@test.com.test","address":""}},"raisedBy":"brown@test.com.test","raisedOn":"2024-02-15T13:23:57.287Z"}'
 }, {
   partitionKey: '123456',
   SessionId: '789123456',
   EventRaised: new Date().toISOString(),
   EventType: 'ineligibility-event',
-  Payload: '{"type":"ineligibility-event","message":"Apply: LockedBusinessError","data":{"sbi":"106363424","crn":"1100514988","exception":"LockedBusinessError","raisedAt":"2024-02-15T13:23:39.830Z","journey":"apply","reference":"TEMP-EFF4-B965"},"raisedBy":"trevorhalec@elahroverts.com.test","raisedOn":"2024-02-15T13:23:40.068Z"}'
+  Payload: '{"type":"ineligibility-event","message":"Apply: LockedBusinessError","data":{"sbi":"123456","crn":"123456789","exception":"LockedBusinessError","raisedAt":"2024-02-15T13:23:39.830Z","journey":"apply","reference":"TEMP-1234-ABCD"},"raisedBy":"brown@test.com.test","raisedOn":"2024-02-15T13:23:40.068Z"}'
 },
 {
   partitionKey: '123456',
   SessionId: '789123456',
   EventRaised: new Date().toISOString(),
   EventType: 'endemicsClaim-organisation',
-  Payload: '{"type":"endemicsClaim-organisation","message":"Session set for endemicsClaim and organisation.","data":{"organisation":{"sbi":"200523665","farmerName":"Thomas Hoggarth","name":"Mossy Lea Farm LLP","email":"thomashoggarthj@htraggohsamohtk.com.test","address":"APPLEBY-IN-WESTMORLAND,EX18 7SB,United Kingdom"}},"raisedBy":"thomashoggarthj@htraggohsamohtk.com.test","raisedOn":"2024-03-05T15:57:39.590Z"}'
+  Payload: '{"type":"endemicsClaim-organisation","message":"Session set for endemicsClaim and organisation.","data":{"organisation":{"sbi":"123456","farmerName":"Farmer Brown","name":"Brown Cow Farm","email":"brown@test.com.test","address":"Yorkshire Moors,AB1 1AB,United Kingdom"}},"raisedBy":"brown@test.com.test","raisedOn":"2024-03-05T15:57:39.590Z"}'
 },
 {
   partitionKey: '123456',
   SessionId: '789123456',
   EventRaised: new Date().toISOString(),
   EventType: 'farmerApplyData-declaration',
-  Payload: '{"type":"farmerApplyData-declaration","message":"Session set for farmerApplyData and declaration.","data":{"reference":"TEMP-5D55-C375","declaration":true},"raisedBy":"thomashoggarthj@htraggohsamohtk.com.test","raisedOn":"2024-01-04T21:27:23.530Z"}'
+  Payload: '{"type":"farmerApplyData-declaration","message":"Session set for farmerApplyData and declaration.","data":{"reference":"TEMP-1234-ABCD","declaration":true},"raisedBy":"brown@test.com.test","raisedOn":"2024-01-04T21:27:23.530Z"}'
 }
 ]
 
@@ -42,22 +42,22 @@ describe('events are transformed to remove json structure', () => {
   })
 
   test('csv content includes sample data from event - farmerApplyData-organisation', () => {
-    const expectedTransformedJsonExample1 = '123456,789123456,farmerApplyData-organisation,Session set for farmerApplyData and organisation.,TEMP-931B-C490'
+    const expectedTransformedJsonExample1 = '123456,789123456,farmerApplyData-organisation,Session set for farmerApplyData and organisation.,TEMP-1234-ABCD,,123456,Farmer Brown,Brown Cow Farm,brown@test.com.test,,brown@test.com.test,2024-02-15T13:23:57.287Z,,,,,,,,,,,,,,,,'
     expect(result).toContain(expectedTransformedJsonExample1)
   })
 
   test('csv content includes sample data from event - ineligibility-event', () => {
-    const expectedTransformedJsonExample2 = '123456,789123456,ineligibility-event,Apply: LockedBusinessError,TEMP-EFF4-B965,,,,,,,trevorhalec@elahroverts.com.test,2024-02-15T13:23:40.068Z,apply,,,,,,,,,,,,,,,'
+    const expectedTransformedJsonExample2 = '123456,789123456,ineligibility-event,Apply: LockedBusinessError,TEMP-1234-ABCD,,,,,,,brown@test.com.test,2024-02-15T13:23:40.068Z,apply,,,,,,,,,,,,,,,'
     expect(result).toContain(expectedTransformedJsonExample2)
   })
 
   test('csv content includes sample data from event - endemicsClaim-organisation', () => {
-    const expectedTransformedJsonExample3 = '123456,789123456,endemicsClaim-organisation,Session set for endemicsClaim and organisation.,,,200523665,Thomas Hoggarth,Mossy Lea Farm LLP,thomashoggarthj@htraggohsamohtk.com.test,APPLEBY-IN-WESTMORLAND  EX18 7SB  United Kingdom,thomashoggarthj@htraggohsamohtk.com.test,2024-03-05T15:57:39.590Z,,,,,,,,,,,,,,,,'
+    const expectedTransformedJsonExample3 = '123456,789123456,endemicsClaim-organisation,Session set for endemicsClaim and organisation.,,,123456,Farmer Brown,Brown Cow Farm,brown@test.com.test,Yorkshire Moors  AB1 1AB  United Kingdom,brown@test.com.test,2024-03-05T15:57:39.590Z,,,,,,,,,,,,,,,,'
     expect(result).toContain(expectedTransformedJsonExample3)
   })
 
   test('csv content includes sample data from event - farmerApplyData-declaration', () => {
-    const expectedTransformedJsonExample3 = '123456,789123456,farmerApplyData-declaration,Session set for farmerApplyData and declaration.,TEMP-5D55-C375,,,,,,,thomashoggarthj@htraggohsamohtk.com.test,2024-01-04T21:27:23.530Z,,,,true,,,,,,,,,,,,'
+    const expectedTransformedJsonExample3 = '123456,789123456,farmerApplyData-declaration,Session set for farmerApplyData and declaration.,TEMP-1234-ABCD,,,,,,,brown@test.com.test,2024-01-04T21:27:23.530Z,,,,true,,,,,,,,,,,,'
     expect(result).toContain(expectedTransformedJsonExample3)
   })
 })
