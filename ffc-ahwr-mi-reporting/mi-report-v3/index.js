1395a0752d4082e9b477836cf1ff76e8aed7c9df
JulieLev <JulieLev@users.noreply.github.com>
2024-05-16 15:40:45 +0100

    added v3 function

---
+++ ffc-ahwr-mi-reporting/mi-report-v3/index.js
@@ -0,0 +1,40 @@
+const config = require('../config/config')
+const createFileName = require('../csv/create-csv-filename')
+const storage = require('../storage/storage')
+const msGraph = require('../sharepoint/ms-graph')
+const transformJsonToCsvV3 = require('./transformJsonToCsvV3')
+
+const buildAhwrMiReportV3 = async (events) => {
+  const fileName = createFileName('ahwr-mi-report-v3-')
+  const dstFolder = `${config.sharePoint.dstFolder}/${config.environment}/${new Date().getFullYear()}/${(new Date().getMonth() + 1).toString().padStart(2, '0')}`
+  if (config.featureToggle.sharePoint.enabled) {
+    console.log(`${new Date().toISOString()} Creating, storing and uploading AHWR MI Report V3: ${JSON.stringify({
+      dstFolder,
+      fileName
+    })}`)
+  } else {
+    console.log(`${new Date().toISOString()} Creating, storing but not uploading AHWR MI Report V3: ${JSON.stringify({
+      fileName
+    })}`)
+  }
+  const csvData = transformJsonToCsvV3(events)
+  if (csvData.length === 0) {
+    console.log(`${new Date().toISOString()} No data found to create: ${JSON.stringify({ fileName })}`)
+    return
+  }
+  await storage.writeFile(fileName, csvData)
+  if (config.featureToggle.sharePoint.enabled) {
+    const fileContent = await storage.downloadFile(fileName)
+    await msGraph.uploadFile(dstFolder, fileName, fileContent)
+    console.log(`${new Date().toISOString()} AHWR MI Report V2 has been stored and uploaded: ${JSON.stringify({
+      dstFolder,
+      fileName
+    })}`)
+  } else {
+    console.log(`${new Date().toISOString()} AHWR MI Report V2 has been stored but not uploaded: ${JSON.stringify({
+      fileName
+    })}`)
+  }
+}
+
+module.exports = buildAhwrMiReportV3
