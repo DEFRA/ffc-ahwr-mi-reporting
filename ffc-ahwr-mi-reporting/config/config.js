d74280fcd977fe2e672fdb43b40f92fb4b5b1718
Mathew Attlee <hello@codeinabox.com>
2026-05-07 18:48:55 +0100

    feat: AHWR-1845 validate config

--- ffc-ahwr-mi-reporting/config/config.js
+++ ffc-ahwr-mi-reporting/config/config.js
@@ -1,11 +1,30 @@
+const Joi = require('joi')
 const featureToggle = require('../feature-toggle/config')
 
-module.exports = {
-  environment: process.env.ENVIRONMENT ?? 'unknown',
+const schema = Joi.object({
+  environment: Joi.string().default('unknown'),
+  connectionString: Joi.string().required(),
+  containerName: Joi.string().default('reports'),
+  tableName: Joi.string().default('ahwreventstore'),
+  sharePoint: Joi.object().default({}),
+  featureToggle: Joi.object().required(),
+  pageSize: Joi.number().integer().min(1).default(1000)
+})
+
+const config = {
+  environment: process.env.ENVIRONMENT,
   connectionString: process.env.STORAGE_CONNECTION_STRING,
-  containerName: 'reports',
-  tableName: 'ahwreventstore',
-  sharePoint: featureToggle.sharePoint.enabled ? require('../sharepoint/config') : {},
+  sharePoint: featureToggle.sharePoint.enabled ? require('../sharepoint/config') : undefined,
   featureToggle,
-  pageSize: Number(process.env.PAGE_SIZE ?? 1000)
+  pageSize: process.env.PAGE_SIZE
 }
+
+const result = schema.validate(config, {
+  abortEarly: false
+})
+
+if (result.error) {
+  throw new Error(`The config is invalid: ${result.error.message}`)
+}
+
+module.exports = result.value
--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,3 @@
 jest.setTimeout(20000)
+
+process.env.STORAGE_CONNECTION_STRING = process.env.STORAGE_CONNECTION_STRING || 'UseDevelopmentStorage=true'
